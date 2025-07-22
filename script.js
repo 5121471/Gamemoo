8a1b393dcc2c8d53392ff4f94d63229abb94bda1
5121471 <saimabbas955@gmail.com>
2025-07-22 05:20:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 const games = {
   happy: [
     {
-      name: "Candy Crush Saga",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.king.candycrushsaga"
+      name: "Stardew Valley",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdtlURqA9c41cX4TbqLfiMfQ-mCWkVwZ_X26v5VgMoMhyBDBoQSucIWrJrWTxWuUulbDUFeg",
+      link: "https://www.stardewvalley.net/"
     },
     {
-      name: "Subway Surfers",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.kiloo.subwaysurf"
+      name: "Overcooked 2",
+      image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTUpr-GhMj1OcRXGrS3hf-2DyauxGEGwJdZ4ez8xzuUgfJs8d8xFcyYxrz6NcJWidEEzxFEuA",
+      link: "https://store.steampowered.com/app/728880/Overcooked_2/"
     }
   ],
   sad: [
     {
-      name: "Monument Valley",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.ustwo.monumentvalley"
+      name: "Journey",
+      image: "https://s3.amazonaws.com/gameopedia_covers/covers/313161/19b7855570aa47598e67d96db7077583.jpeg",
+      link: "https://store.steampowered.com/app/638230/Journey/"
     }
   ],
   angry: [
     {
-      name: "Angry Birds 2",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.rovio.baba"
+      name: "DOOM Eternal",
+      image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQvZcfTRV7HhomN0-2pG-cBZ6VREhe2Yx21RE9YKGoYsv-TWGhrT0ruDwfm_OcL-kvvvA0iYw",
+      link: "https://store.steampowered.com/app/782330/DOOM_Eternal/"
     }
   ],
   bored: [
     {
-      name: "Ludo King",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.ludo.king"
+      name: "Minecraft",
+      image: "https://i.pcmag.com/imagery/reviews/05OVdVevn5X6gC6uMIOcsLN-1.fit_scale.size_400x225.v1569481779.jpg",
+      link: "https://www.minecraft.net/en-us"
     }
   ],
   adventurous: [
     {
-      name: "PUBG MOBILE",
-      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
-      link: "https://play.google.com/store/apps/details?id=com.tencent.ig"
+      name: "The Legend of Zelda: Breath of the Wild",
+      image: "images/zelda.jpg",
+      link: "https://www.zelda.com/breath-of-the-wild/"
     }
   ]
 };
@@ -54,7 +54,6 @@ function showGames(mood) {
     `;
   });
 }
-
 function searchGames() {
   const query = document.getElementById("search-input").value.toLowerCase();
   const gameList = document.getElementById("game-list");

5855b0cb991e3a55d12825406f72a2a0f5f88613
5121471 <saimabbas955@gmail.com>
2025-07-22 05:39:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ const games = {
   happy: [
     {
       name: "Stardew Valley",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdtlURqA9c41cX4TbqLfiMfQ-mCWkVwZ_X26v5VgMoMhyBDBoQSucIWrJrWTxWuUulbDUFeg",
-      link: "https://www.stardewvalley.net/"
+      image: "images/stardew.jpg",
+      link: "https://play.google.com/store/apps/details?id=com.chucklefish.stardewvalley"
     },
     {
       name: "Overcooked 2",
-      image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTUpr-GhMj1OcRXGrS3hf-2DyauxGEGwJdZ4ez8xzuUgfJs8d8xFcyYxrz6NcJWidEEzxFEuA",
-      link: "https://store.steampowered.com/app/728880/Overcooked_2/"
+      image: "images/overcooked2.jpg",
+      link: "https://play.google.com/store/search?q=overcooked%202&c=apps"
     }
   ],
   sad: [
     {
       name: "Journey",
-      image: "https://s3.amazonaws.com/gameopedia_covers/covers/313161/19b7855570aa47598e67d96db7077583.jpeg",
-      link: "https://store.steampowered.com/app/638230/Journey/"
+      image: "images/journey.jpg",
+      link: "https://play.google.com/store/search?q=journey&c=apps"
     }
   ],
   angry: [
     {
       name: "DOOM Eternal",
-      image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQvZcfTRV7HhomN0-2pG-cBZ6VREhe2Yx21RE9YKGoYsv-TWGhrT0ruDwfm_OcL-kvvvA0iYw",
-      link: "https://store.steampowered.com/app/782330/DOOM_Eternal/"
+      image: "images/doometernal.jpg",
+      link: "https://play.google.com/store/search?q=doom%20eternal&c=apps"
     }
   ],
   bored: [
     {
       name: "Minecraft",
-      image: "https://i.pcmag.com/imagery/reviews/05OVdVevn5X6gC6uMIOcsLN-1.fit_scale.size_400x225.v1569481779.jpg",
-      link: "https://www.minecraft.net/en-us"
+      image: "images/minecraft.jpg",
+      link: "https://play.google.com/store/apps/details?id=com.mojang.minecraftpe"
     }
   ],
   adventurous: [
     {
       name: "The Legend of Zelda: Breath of the Wild",
       image: "images/zelda.jpg",
-      link: "https://www.zelda.com/breath-of-the-wild/"
+      link: "https://play.google.com/store/search?q=zelda&c=apps"
     }
   ]
 };

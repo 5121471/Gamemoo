544cedc3f0f8761fbdcc3fc7998fb2b925ed2aca
5121471 <saimabbas955@gmail.com>
2025-07-22 06:02:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,137 @@
 const games = {
   happy: [
     {
+      name: "Candy Crush Saga",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.king.candycrushsaga"
+    },
+    {
+      name: "Subway Surfers",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.kiloo.subwaysurf"
+    },
+    {
+      name: "Temple Run",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.imangi.templerun"
+    },
+    {
+      name: "Fruit Ninja",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.halfbrick.fruitninjafree"
+    },
+    {
       name: "Stardew Valley",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdtlURqA9c41cX4TbqLfiMfQ-mCWkVwZ_X26v5VgMoMhyBDBoQSucIWrJrWTxWuUulbDUFeg",
       link: "https://play.google.com/store/apps/details?id=com.chucklefish.stardewvalley"
-    },
-    {
-      name: "Overcooked 2",
-      image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTUpr-GhMj1OcRXGrS3hf-2DyauxGEGwJdZ4ez8xzuUgfJs8d8xFcyYxrz6NcJWidEEzxFEuA",
-      link: "https://play.google.com/store/search?q=overcooked%202&c=apps"
     }
   ],
   sad: [
     {
+      name: "Monument Valley",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.ustwo.monumentvalley"
+    },
+    {
+      name: "Alto's Odyssey",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.noodlecake.altosodyssey"
+    },
+    {
       name: "Journey",
       image: "https://s3.amazonaws.com/gameopedia_covers/covers/313161/19b7855570aa47598e67d96db7077583.jpeg",
       link: "https://play.google.com/store/search?q=journey&c=apps"
+    },
+    {
+      name: "Florence",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.mountains.florence"
+    },
+    {
+      name: "My Oasis",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.buffstudio.myoasis"
     }
   ],
   angry: [
     {
+      name: "Angry Birds 2",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.rovio.baba"
+    },
+    {
+      name: "Shadow Fight 3",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.nekki.shadowfight3"
+    },
+    {
       name: "DOOM Eternal",
       image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQvZcfTRV7HhomN0-2pG-cBZ6VREhe2Yx21RE9YKGoYsv-TWGhrT0ruDwfm_OcL-kvvvA0iYw",
       link: "https://play.google.com/store/search?q=doom%20eternal&c=apps"
+    },
+    {
+      name: "Brawl Stars",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.supercell.brawlstars"
+    },
+    {
+      name: "Stickman Legends",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.zitga.stickman.legend.shadow.fight"
     }
   ],
   bored: [
     {
+      name: "Ludo King",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.ludo.king"
+    },
+    {
       name: "Minecraft",
       image: "https://i.pcmag.com/imagery/reviews/05OVdVevn5X6gC6uMIOcsLN-1.fit_scale.size_400x225.v1569481779.jpg",
       link: "https://play.google.com/store/apps/details?id=com.mojang.minecraftpe"
+    },
+    {
+      name: "Among Us",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.innersloth.spacemafia"
+    },
+    {
+      name: "UNO!™",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.matteljv.uno"
+    },
+    {
+      name: "Brain Test: Tricky Puzzles",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.unicostudio.braintest"
     }
   ],
   adventurous: [
     {
+      name: "PUBG MOBILE",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.tencent.ig"
+    },
+    {
+      name: "Free Fire",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.dts.freefireth"
+    },
+    {
+      name: "Asphalt 9: Legends",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.gameloft.android.ANMP.GloftA9HM"
+    },
+    {
       name: "The Legend of Zelda: Breath of the Wild",
       image: "https://upload.wikimedia.org/wikipedia/en/0/0d/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
       link: "https://play.google.com/store/search?q=zelda&c=apps"
+    },
+    {
+      name: "Genshin Impact",
+      image: "https://play-lh.googleusercontent.com/8QkQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQwQw=w240-h480-rw",
+      link: "https://play.google.com/store/apps/details?id=com.miHoYo.GenshinImpact"
     }
   ]
 };
